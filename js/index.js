142faf531e4c1dfe97dfd94a38a345698d383072
behouba <behouba@gmail.com>
2018-05-22 03:12:00 +0300

    portfolio section and contact section almost done

--- js/index.js
+++ js/index.js
@@ -1,27 +1,13 @@
-$(document).ready(function(){
-  $("#about").click(function(){
-     $('html, body').animate({
-        scrollTop: $( $(this).attr('href') ).offset().top
-    }, 1000);
-    return false;
-  })
-  $("#portfolio").click(function(){
-     $('html, body').animate({
-        scrollTop: $( $(this).attr('href') ).offset().top
-    }, 2000);
-    return false;
-  })
-  $("#contact").click(function(){
-     $('html, body').animate({
-        scrollTop: $( $(this).attr('href') ).offset().top
-    }, 2000);
-    return false;
-  })
-  
-  $("#Top").mouseout(function(){
-    $("#Top").css('opacity','0.9')
-  })
-  $("#Top").mouseover(function(){
-    $("#Top").css('opacity','1')
-  })
-})
+function sendEmail() {
+  var form = document.getElementById("contact_form");
+  var subjet = document.getElementById("subjet").value;
+  var name = document.getElementById("name").value;
+  var body = document.getElementById("body").value;
+  console.log(body.length)
+  if (body !== "") {
+    form.action = `mailto:behouba@gmail.com?subject=${subjet}&body=${body}`;
+    form.submit();
+    return;
+  }
+  alert("Veillez ecrire votre message s'il vous plait");
+}
